a8fa39f794909f6792a387d8a676572ea8b46d08
Tech-Applywizz-git-account <techteam@Applywizz.com>
2026-05-08 18:23:43 +0530

    Final Submission: Added real-time chat, task visibility, and email notifications-3

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Message, Attachment
-from accounts.serializers import UserMinimalSerializer
+from accounts.serializers import UserSerializer
 
 class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,7 +8,7 @@ class AttachmentSerializer(serializers.ModelSerializer):
         fields = ['id', 'file', 'file_name', 'file_type', 'file_size']
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserMinimalSerializer(read_only=True)
+    sender = UserSerializer(read_only=True)
     attachments = AttachmentSerializer(many=True, read_only=True)
     
     class Meta:
